ff33ba32e0ea4e0976811b2aaeb168b7a78bc65b
Andreugc19 <agarcia20507@iesjoanramis.org>
2024-03-05 16:18:08 +0100

    Escape Room Final Andreu Garcia

--- Registre/registre.js
+++ Registre/registre.js
@@ -2,7 +2,17 @@ document.getElementById('registro-form').addEventListener('submit', function(eve
     event.preventDefault();
     var nombreUsuario = document.getElementById('nombre').value;
     var contrasena = document.getElementById('contrasena').value;
-    
+
+    // Validar el nombre de usuario
+    var nombreValido = validarNombreUsuario(nombreUsuario);
+
+    // Validar la contraseña
+    var contrasenaValida = validarContrasena(contrasena);
+
+    if (!nombreValido || !contrasenaValida) {
+        return; // Detener el proceso si los campos no son válidos
+    }
+
     // Verificar si el nombre de usuario ya existe en el almacenamiento local
     if (localStorage.getItem(nombreUsuario)) {
         document.getElementById('mensaje').textContent = 'El nombre de usuario ya está en uso.';
@@ -13,3 +23,27 @@ document.getElementById('registro-form').addEventListener('submit', function(eve
         document.getElementById('mensaje').textContent = 'Usuario registrado con éxito.';
     }
 });
+
+function validarNombreUsuario(nombreUsuario) {
+    var nombreError = document.getElementById('nombre-error');
+    var regex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    if (!regex.test(nombreUsuario)) {
+        nombreError.textContent = 'El nombre de usuario debe tener al menos 6 caracteres y contener al menos 1 mayúscula y 1 minúscula.';
+        return false;
+    } else {
+        nombreError.textContent = '';
+    }
+    return true;
+}
+
+function validarContrasena(contrasena) {
+    var contrasenaError = document.getElementById('contrasena-error');
+    var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    if (!regex.test(contrasena)) {
+        contrasenaError.textContent = 'La contraseña debe tener al menos 8 caracteres, 1 mayúscula, 1 minúscula y 1 número.';
+        return false;
+    } else {
+        contrasenaError.textContent = '';
+    }
+    return true;
+}
